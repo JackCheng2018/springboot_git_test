983adfa3bbaba49ab8bb5114ba7635e5b9557158
JackCheng2018 <maszxy@126.com>
2022-11-23 17:15:13 +0800

    Init Project

---
+++ src/test/java/com/jack/springbootgittest/SpringBootGitTestApplicationTests.java
@@ -0,0 +1,14 @@
+package com.jack.springbootgittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGitTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("Just do test");
+    }
+
+}
